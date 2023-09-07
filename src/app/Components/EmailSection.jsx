443198a5c6984e99f200d5ab3173c453ba899c6b
Mennaamohamed25 <Mennaa.mohamedd25@gmail.com>
2023-09-07 05:41:39 +0300

    first commit

---
+++ src/app/Components/EmailSection.jsx
@@ -0,0 +1,123 @@
+'use client';
+import React, {useState} from 'react';
+import GithubIcon from "../../../public/Images/github-icon.svg";
+import LinkedinIcon from "../../../public/Images/linkedin-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+const EmailSection = () => {
+
+  const [emailSubmited, setEmailSubmited] = useState(false);
+// HANDEL SUB FUNCTION
+
+const handelSubmitted =async (e) =>{
+  // To PREVENT DEFAULT OF SUB BUTTON
+  e.preventDefault();  
+
+  const data = {
+    email: e.target.email.value,
+    message: e.target.message.value,
+    email: e.target.email.value,
+  }
+  const JSONdata = JSON.stringify(data);
+  const endPoint = '/api/send';
+
+  const option = {
+
+    method: 'POST',
+
+    headers: {
+      'Content-Type':'application/json',
+
+    },
+
+    body : JSONdata,
+  }
+
+  const response = await fetch(endPoint ,option );
+  const resData = await response.json();
+  // console.log(resData)
+  if (response.status === 200){
+    setEmailSubmited(true);
+  }
+}
+
+  return (
+   <section id="contact"
+   className="grid md:grid-cols-2 my-12  py-24 gap-4 relative">
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 left-4 transform -translate-x-1/2 -translate-1/2"></div>
+    {/* INFO AND ICONS PART */}
+    <div className="z-10">
+        <h5 className='text-xl font-bold text-white my-2'> Let's Connect </h5> 
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>I'm currently looking for a new opportunity, my inbox is always open.
+          Whether you have a question or  want to hire me, I will
+          try my best to get back to you!
+        </p>
+        <div className='flex flex-row gap-2'>
+        <Link href="https://www.linkedin.com/in/menna-muhamed-643250173/">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+        <Link href="https://github.com/Mennaamohamed25">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+          
+        </div>
+    </div>
+    {/* EMAIL FORM */}
+    <div className="z-10">
+        <form className='flex flex-col' onSubmit={handelSubmitted}>
+
+          <div className='mb-6'>
+          <label htmlFor='email' className='text-white block mb-2 text-sm font-medium '>Your Email </label>
+            <input type='email' id='email' name='email' required placeholder='example@gmail.com' className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'/> 
+          </div>
+
+
+          <div className='mb-6'>  
+            <label htmlFor='subject' className='text-white block text-sm mb-2 font-medium'>Subject </label>
+            <input type='text' id='subject' name='subject' required placeholder='Enter your subject' className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'/> 
+            </div>
+
+
+            <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Let's talk about..."
+            />
+          </div>
+
+
+          <button
+
+            type="submit"
+            className="bg-purple-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full cursor-not-allowed"
+            disabled
+          >
+            Send Message
+          </button>
+          {/* IF EMAIL IS SUB  */}
+{
+  emailSubmited && (
+    <p className='text-green-500 text-sm mt-2 text-center'>
+      Email sent successfully!
+    </p>
+  ) 
+
+
+}
+        </form>
+    </div>
+
+   </section>
+  )
+}
+
+export default EmailSection
---
+++ src/app/Components/ProjectsSection.jsx
@@ -0,0 +1,137 @@
+"use client";
+import React , {useState , useRef} from 'react';
+import ProjectsCard from './ProjectsCard';
+import ProjectLinks from './ProjectLinks';
+import { motion, useInView } from "framer-motion";
+// Project Data
+const projectsData = [
+    {
+        id : 1,
+        title : 'Movies App',
+        desc : 'Movies App using react.js to get the daily or weekly trending items. Include all movies, TV shows people integrated with themoviedb Api Using React-router-dom. Movie DB API and fetch data by Axios. Context and ReactHooks.',
+        image : '/Images/prj/movie.png',
+        tag :['All' , 'React'],
+        gitUrl : 'https://github.com/Mennaamohamed25/Movies-React-App-a',
+        liveUrl : 'https://mennaamohamed25.github.io/Movies-React-App-a/',
+    },
+    {
+        id : 2,
+        title : 'Weather Website',
+        desc : 'This website displays the weather for three days using the API, and you can also search for any country to see its weather. Using javaScript , HTML and CSS.',
+        image : "/Images/prj/weather.png",
+        tag :['All' , 'JavaScript'],
+        gitUrl : 'https://github.com/Mennaamohamed25/Weather-App',
+        liveUrl : 'https://mennaamohamed25.github.io/Weather-App/',
+    },
+    {
+        id : 3,
+        title : 'CRUD',
+        desc : 'This is a simple product management system using HTML, CSS, Bootstrap, and JavaScript. This project incorporates the fundamental principles of CRUD operations, enabling you to perform essential tasks such as adding products, searching for products, modifying product information, and deleting products.',
+        image : '/Images/prj/crud.png',
+        tag :['All' , 'JavaScript'],
+        gitUrl : 'https://github.com/Mennaamohamed25/CRUD_Project.git',
+        liveUrl : 'https://mennaamohamed25.github.io/CRUD/',
+    },
+    {
+        id : 4,
+        title : 'My Portfolio',
+        desc : 'This is a portfolio using Next js , Tailwind css , Hooks , Resend next js API , Framer-motion , React-animated-numbers and React-type-animation .',
+        image : '/Images/prj/mypro.png',
+        tag :['All' , 'Next'],
+        gitUrl : 'https://github.com/Mennaamohamed25/Myprofile.git',
+        liveUrl : '/',
+    },
+
+  
+    {
+        id : 5,
+        title : 'Bookmark',
+        desc : 'Bookmark website is a local storage and some features like visit , edit and delete HTML5, CSS3, Bootstrap5, Javascript, ES6+ and Regular Expression',
+        image : '/Images/prj/bookmark.png',
+        tag :['All' , 'JavaScript' ],
+        gitUrl : 'https://github.com/Mennaamohamed25/Bookmark.git',
+        liveUrl : 'https://mennaamohamed25.github.io/Bookmark/',
+    },
+    {
+        id : 6,
+        title : 'Simple Portfolio',
+        desc : 'It is a simple responsive portfolio using HTML , CSS , Bootstrap.',
+        image : '/Images/prj/pro.png',
+        tag :['All' ],
+        gitUrl : 'https://github.com/Mennaamohamed25/Designer-Profile',
+        liveUrl : 'https://mennaamohamed25.github.io/Designer-Profile/',
+    },
+    {
+        id : 7,
+        title : 'Quote website',
+        desc : 'This website is about the quoute of the day. using HTML , CSS , Bootstrap and javaScript',
+        image : '/Images/prj/qoute.png',
+        tag :['All' , 'JavaScript' ],
+        gitUrl : 'https://github.com/Mennaamohamed25/Quote',
+        liveUrl : 'https://mennaamohamed25.github.io/Quote/',
+    },
+
+ 
+];
+
+const ProjectsSection = () => {
+    const [tag, setTag] = useState('All');
+
+    const ref = useRef(null);
+
+    const isInView = useInView(ref, { once: true });
+    // Function to handle the links or tag
+
+    const handelLinkChange = (newLink) => {
+        setTag(newLink)
+    };
+
+    // Function to Filter the projects
+
+    const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+//   MOTIONS
+const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id='projects'>
+<h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>My Projects</h2>
+
+<div className='text-white flex flex-col sm:flex-row justify-center items-center gap-2 py-6 '>
+
+<ProjectLinks onClick={handelLinkChange} name='All' isSelected={tag === 'All'} />
+
+<ProjectLinks onClick={handelLinkChange} name='React' isSelected={tag === 'React'} />
+
+<ProjectLinks onClick={handelLinkChange} name='Next' isSelected={tag === 'Next'} />
+
+<ProjectLinks onClick={handelLinkChange} name='JavaScript' isSelected={tag === 'JavaScript'} />
+
+</div>
+
+
+<ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+    {filteredProjects.map((project , index) => (
+   
+   <motion.li
+   key={index}
+   variants={cardVariants}
+   initial="initial"
+   animate={isInView ? "animate" : "initial"}
+   transition={{ duration: 0.3, delay: index * 0.4 }}
+ >
+
+    <ProjectsCard key={project.id} title={project.title} desc={project.desc} imgUrl={project.image} gitUrl={project.gitUrl} liveUrl={project.liveUrl} />
+    </motion.li>
+    ))}
+</ul>
+    </section>
+  )
+}
+
+export default ProjectsSection
