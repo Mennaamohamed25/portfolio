86e5e13a79cbe65cbabf5624dd40455130d1174e
Mennaamohamed25 <Mennaa.mohamedd25@gmail.com>
2023-09-07 08:30:51 +0300

    change21

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
    <motion.div initial={{opacity:0 , scale:0.3}} animate={{opacity:1 , scale:1}} transition={{duration:0.3}} className='col-span-5 place-self-center mt-4 lg:mt-0'>
    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
